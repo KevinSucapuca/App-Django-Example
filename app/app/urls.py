9a3542da361c275012fa315da06f3f4c2a98c662
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-29 00:00:45 -0500

    creando vista helloworld

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from series.views import HelloWorld
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HelloWorld.as_view())
+    
 ]
--- app/series/views.py
+++ app/series/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse,request
 from django.shortcuts import render
 
+
 # Create your views here.
+from django.views.generic.base import View
+
+
+class HelloWorld(View):
+
+    
+    def get(self, request):
+        return HttpResponse(content = b'Hello World')
