361213ce80872942259abd557cd8832bb240164f
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-29 00:28:05 -0500

    add models Serie and Episodie

--- app/series/models.py
+++ app/series/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models import CASCADE
 
 # Create your models here.
+class Serie(models.Model):
+    title = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+class Episodie(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=100)
+    serie = models.ForeignKey(Serie, on_delete=CASCADE)
