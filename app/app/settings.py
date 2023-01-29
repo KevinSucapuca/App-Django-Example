f4dc36abe7882c54c09dce5243b60493dfa7ec06
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-28 23:42:13 -0500

    modificando settings.py-Language_code

--- app/app/settings.py
+++ app/app/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-PE'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Latin America/Bogota'
 
 USE_I18N = True
 
