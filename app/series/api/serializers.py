39e389c046144347a87ca753fe4a8876abc5f1c6
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-30 13:52:06 -0500

    add validationError serializer

--- app/series/api/serializers.py
+++ app/series/api/serializers.py
@@ -1,9 +1,24 @@
 
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from series.models import Serie
+
+
+from typing import (
+    
+    Dict,
+    
+)
+
+
+
 class SerieSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     
+    def validate(self, serie: Dict[str,str]):
+        if not serie.get('title'):
+            raise ValidationError('Título no válido')
+
     class Meta:
         model = Serie
         fields = '__all__'
