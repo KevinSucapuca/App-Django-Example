02db0720af758c10b02e7c1af652080f38fba23f
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-30 12:50:43 -0500

    add apiviews

---
+++ app/series/api/views.py
@@ -0,0 +1,22 @@
+
+from rest_framework.views import APIView, Response
+from django.http import HttpResponse,request,status
+from series.models import Serie
+
+
+
+class SerieApiView(APIView):
+    
+    def get (self, request):
+        series = [serie.title for serie in Serie.objects.all()]
+        return Response(data= series, status=status.HTTP_200_OK)
+    
+    def post (self, request):
+        Serie.objects.create(title=request.POST['title'], descripcion=request.POST['descripcion'])
+        
+
+        return self.get(request)
+
+        
+
+    
