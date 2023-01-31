27d12b0002c4dc38a415e3ae952057d801042254
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-30 21:24:28 -0500

    add ModelViewSet

--- app/series/api/views.py
+++ app/series/api/views.py
@@ -1,6 +1,6 @@
 
 from rest_framework.views import  Response,status
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 from series.models import Serie
 
 from .serializers import SerieSerializer
@@ -9,21 +9,23 @@ from .serializers import SerieSerializer
 
 
 #class SerieApiView(APIView):
-class SerieApiView(ViewSet):  
+#class SerieApiView(ViewSet):  
+
 
     #def get (self, request):
         #series = SerieSerializer(Serie.objects.all(), many = True)
 
         #return Response(data= series.data, status=status.HTTP_200_OK)
-    def list (self, request):
-        series = SerieSerializer(Serie.objects.all(), many = True)
+    #Def ViewSet
+    #def list (self, request):
+        #series = SerieSerializer(Serie.objects.all(), many = True)
 
-        return Response(data= series.data, status=status.HTTP_200_OK)
+        #return Response(data= series.data, status=status.HTTP_200_OK)
     
-    def retrieve (self, request, pk: int):
-        series = SerieSerializer(Serie.objects.get(pk=pk))
+    #def retrieve (self, request, pk: int):
+        #series = SerieSerializer(Serie.objects.get(pk=pk))
 
-        return Response(data= series.data, status=status.HTTP_200_OK)
+        #return Response(data= series.data, status=status.HTTP_200_OK)
     
     
     
@@ -32,9 +34,13 @@ class SerieApiView(ViewSet):
         #serie.is_valid(raise_exception=True)
         #Serie.objects.create(title=serie.validated_data['title'], descripcion=serie.validated_data['descripcion'])
         #return self.get(request)
-
-    def create (self, request):
-        serie = SerieSerializer(data=request.POST)
-        serie.is_valid(raise_exception=True)
-        Serie.objects.create(title=serie.validated_data['title'], descripcion=serie.validated_data['descripcion'])
-        return self.list(request)
+    ##def ViewSet
+    #def create (self, request):
+        #serie = SerieSerializer(data=request.POST)
+        #serie.is_valid(raise_exception=True)
+        #Serie.objects.create(title=serie.validated_data['title'], descripcion=serie.validated_data['descripcion'])
+        #return self.list(request)
+class SerieApiView(ModelViewSet): 
+    queryset = Serie.objects.all()
+    serializer_class = SerieSerializer
+    
