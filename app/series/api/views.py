44b9bcc055d05b92ee477bf76df4646d2dadc749
KevinSucapuca <ksucapuca007@gmail.com>
2023-01-30 19:44:53 -0500

    add setviews serializer

--- app/series/api/views.py
+++ app/series/api/views.py
@@ -1,6 +1,6 @@
 
-from rest_framework.views import APIView, Response,status
-
+from rest_framework.views import  Response,status
+from rest_framework.viewsets import ViewSet
 from series.models import Serie
 
 from .serializers import SerieSerializer
@@ -8,17 +8,33 @@ from .serializers import SerieSerializer
 
 
 
-class SerieApiView(APIView):
-    
-    def get (self, request):
+#class SerieApiView(APIView):
+class SerieApiView(ViewSet):  
+
+    #def get (self, request):
+        #series = SerieSerializer(Serie.objects.all(), many = True)
+
+        #return Response(data= series.data, status=status.HTTP_200_OK)
+    def list (self, request):
         series = SerieSerializer(Serie.objects.all(), many = True)
+
+        return Response(data= series.data, status=status.HTTP_200_OK)
+    
+    def retrieve (self, request, pk: int):
+        series = SerieSerializer(Serie.objects.get(pk=pk))
 
         return Response(data= series.data, status=status.HTTP_200_OK)
     
     
     
-    def post (self, request):
+    #def post (self, request):
+        #serie = SerieSerializer(data=request.POST)
+        #serie.is_valid(raise_exception=True)
+        #Serie.objects.create(title=serie.validated_data['title'], descripcion=serie.validated_data['descripcion'])
+        #return self.get(request)
+
+    def create (self, request):
         serie = SerieSerializer(data=request.POST)
         serie.is_valid(raise_exception=True)
         Serie.objects.create(title=serie.validated_data['title'], descripcion=serie.validated_data['descripcion'])
-        return self.get(request)
+        return self.list(request)
