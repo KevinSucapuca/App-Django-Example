dd274143b663153d23ffc1de3e9fd2f2adb70cb9
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-03 13:25:27 -0500

    modificando para deploy

--- app/app/settings.py
+++ app/app/settings.py
@@ -80,11 +80,18 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER' : config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST' : config('HOST'),
+        'PORT' : config('PORT'), 
     }
+    
 }
 
 
