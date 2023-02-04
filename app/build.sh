0da4c009ca97822865193084783d5d4459eeedd5
KevinSucapuca <ksucapuca007@gmail.com>
2023-02-03 23:32:40 -0500

    modificando build

--- app/build.sh
+++ app/build.sh
@@ -1,11 +1,11 @@
 
 
 echo "Building the project..."
-python3.11 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.11 manage.py makemigrations --noinput 
-python3.11 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput 
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static"
-python3.11 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
